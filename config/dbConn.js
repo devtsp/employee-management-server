b6a240053f929dcdb8f33b7f22cfeab051b79b45
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-09 21:45:30 -0300

    add readme

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-// Mongoose is async
 const connectDB = async () => {
 	try {
-		// process.env = searchs on .env file
 		await mongoose.connect(
 			process.env.DATABASE_URI,
 			// Prevent warning from mongoDB
